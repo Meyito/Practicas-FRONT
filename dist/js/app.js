55817310cb1194d18aea1cb66fcaf863f2a53e65
Angie Melissa Delgado <amelisd@gmail.com>
2016-11-01 15:09:51 -0500

    Vista registro de nueva actividad

--- dist/js/app.js
+++ dist/js/app.js
@@ -392,22 +392,58 @@
 
         $scope.assistants = [];
         $scope.new_activity = {};
-        
 
-        self.saveActivity = function(){
+
+        self.saveActivity = function () {
             $scope.saved = true;
-        } 
+            inform.add("Se ha creado la nueva actividad", { type: "success" });
+        }
 
-        self.addAssistant = function(){
-            $scope.assistants.push( $scope.new_activity );
+        self.addAssistant = function () {
+            $scope.assistants.push($scope.new_activity);
             $scope.new_activity = {};
         }
 
-        
+        self.saveAssistants = function () {
+            inform.add("Se han guardado los asistentes a la actividad", { type: "success" });
+        }
+
+        self.deleteAssistant = function (id) {
+            $scope.assistants.splice(id, 1);
+        }
+
+        self.upload = function () {
+            var modalInstance = $uibModal.open({
+                animation: true,
+                ariaLabelledBy: 'Cargar Asistentes',
+                ariaDescribedBy: 'cargar-asistentes',
+                templateUrl: 'templates/assistants.modal.html',
+                controller: 'ModalController',
+                controllerAs: 'modalCtrl',
+                resolve: {
+                    data: {}
+                }
+            });
+
+            modalInstance.result.then(function (data) {
+                inform.add("Se han cargado los asistentes correctamente.", { type: "success" });
+                
+                /*PlanService.uploadPlan(data.file, d).then(
+                    function (response) {
+                        inform.add("Se ha cargado el plan de desarrollo correctamente", { type: "info" });
+                        //Refrescar todos los planes de desarrollo
+                    }, function (err) {
+                        inform.add("Ocurrió un error al guardar el plan de desarrollo", { type: "warning" });
+                        //Descargar reporte de errores 
+                    }
+                );*/
+            });
+        }
+
 
         self.init = function () {
-            //$scope.saved = false;
-            $scope.saved = true;
+            $scope.saved = false;
+            //$scope.saved = true;
         }
 
         self.init();
